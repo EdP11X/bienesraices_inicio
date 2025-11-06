7b5773b9201173b5b947f495f2341a7ad6e4f86d
EdP11X <laaloponce3@gmail.com>
2025-11-05 20:44:39 -0600

    Implement dark mode feature and responsive navigation
    - Updated dark mode functionality to automatically reflect user's system preference.
    - Added event listener for dark mode toggle button.
    - Refactored HTML structure for navigation and dark mode button in contacto.html, entrada.html, and nosotros.html.
    - Enhanced SCSS styles for dark mode, including adjustments to various components.
    - Improved navigation visibility and transition effects in _navegacion.scss and _footer.scss.

--- src/js/app.js
+++ src/js/app.js
@@ -5,11 +5,32 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function darkmode() {  //se crea la consatante darmode que va a quitar y poner la clase dark mode del body al darle click al icono de luna
+    
+    //Este bloque de codigo sirve para que el dark mode se active automaticamente si el usuario tiene configurado el dark mode en su sistema operativo
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    //console.log(prefiereDarkMode.matches);
+    if(prefiereDarkMode.matches){   
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    //Este bloque de codigo sirve para que el dark mode se active automaticamente si el usuario tiene configurado el dark mode en su sistema operativo
+    prefiereDarkMode.addEventListener('change', function() {
+        if(prefiereDarkMode.matches){   
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
     const botonDarkMode = document.querySelector('.darck-mode-boton'); 
     botonDarkMode.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
     }); 
 }
+
+
 
 function addEventListener() {
     //alert('Hola Mundo');
